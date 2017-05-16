5a965ade835e8b436a6c7a5de7f45c2dd1018b00
디미고인 <@DESKTOP-0UKIU1H>
2017-05-16 14:38:14 +0900

    실습과제 12

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhoneTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 16.
+ * </pre>
+ * 
+ * @author      : 디미고인
+ * @version     : 1.0
+ */
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		SmartPhone[] s1 = {
+				new IPhone("iphone 7","애플",900000),
+				new Galaxy("갤럭시 S8","삼성",800000)
+		};
+		
+		for(SmartPhone p : s1) {
+			System.out.println(p.toString());
+			p.turnOn();
+			p.pay();
+			p.useSpecialFunction(p);
+			p.turnOff();
+		}
+	}
+
+}
